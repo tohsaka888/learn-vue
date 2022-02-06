094807c8a4f9ec4cd89b17d86889e8f1794ff465
tohsaka888 <156132264@qq.com>
2022-02-06 16:45:18 +0800

    feat: catalogue config

---
+++ src/components/catalogue.ts
@@ -0,0 +1,28 @@
+type Catalogue = {
+  title: string;
+  date: string;
+  path: string;
+};
+
+export const catalogue: Catalogue[] = [
+  {
+    title: "组件实例",
+    date: "2022-2-4",
+    path: "/change-color",
+  },
+  {
+    title: "模板语法",
+    date: "2022-2-4",
+    path: "/template-syntax",
+  },
+  {
+    title: "动态参数",
+    date: "2022-2-4",
+    path: "/dynamic-prop",
+  },
+  {
+    title: "Data Property 和方法",
+    date: "2022-2-5",
+    path: "/debouncing-example",
+  },
+];
